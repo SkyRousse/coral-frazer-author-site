dd213058dc75c927e1a9a1539b868f14a072b247
Skyr <skyrousse@gmail.com>
2018-01-22 00:15:15 -0800

    add about page content and styles,  also make header sticky with fade effect and add site images

---
+++ assets/js/main.js
@@ -0,0 +1,88 @@
+
+/** ===========================================
+    Hide / show the master navigation menu
+============================================ */
+
+console.log('Window Height is: ' + $(window).height());
+console.log('Document Height is: ' + $(document).height());
+
+var previousScroll = 0;
+
+$(window).scroll(function () {
+
+    var currentScroll = $(this).scrollTop();
+
+    /*
+        If the current scroll position is greater than 0 (the top) AND the current scroll position is less than the document height minus the window height (the bottom) run the navigation if/else statement.
+    */
+    if (currentScroll > 0 && currentScroll < $(document).height() - $(window).height()) {
+        /*
+            If the current scroll is greater than the previous scroll (i.e we're scrolling down the page), hide the nav.
+        */
+        if (currentScroll > previousScroll) {
+            window.setTimeout(hideNav, 300);
+            /*
+                Else we are scrolling up (i.e the previous scroll is greater than the current scroll), so show the nav.
+            */
+        } else {
+            window.setTimeout(showNav, 300);
+        }
+        /* 
+            Set the previous scroll value equal to the current scroll.
+        */
+        previousScroll = currentScroll;
+    }
+
+});
+
+function hideNav() {
+    $("[data-nav-status='toggle']").removeClass("is-visible").addClass("is-hidden");
+}
+function showNav() {
+    $("[data-nav-status='toggle']").removeClass("is-hidden").addClass("is-visible");
+}
+
+
+
+/** =================================================
+    about page | bio section hide and show photos
+================================================== */
+
+if ($('.about-page').length) {
+    console.log('on the about page');
+    var babyPhoto = $('.about-page').find('img[data-about-photo="baby"]');
+    var babyParagraph = $('.about-page').find('p[data-about-position="1"]');
+    var rainbowPhoto = $('.about-page').find('img[data-about-photo="rainbow"]');
+    var rainbowParagraph = $('.about-page').find('p[data-about-position="2"]');
+    var bookPhoto = $('.about-page').find('img[data-about-photo="book"]');
+    var bookParagraph = $('.about-page').find('p[data-about-position="3"]');
+    var containerHeight = $('.about-page').height();
+    
+    var babyParagraphPos = babyParagraph.offset().top;
+    var rainbowParagraphPos = rainbowParagraph.offset().top;
+    var bookParagraphPos = bookParagraph.offset().top;
+    
+    
+    var myVar = setInterval(function () { aboutPageTimer() }, 500);
+    
+    function aboutPageTimer() {
+        var scrollPosition = $(window).scrollTop();
+        if (babyParagraphPos >= scrollPosition ) {
+            console.log('keep scrolling');
+            $(babyPhoto).fadeIn();
+            $(rainbowPhoto).fadeOut();
+            $(bookPhoto).fadeOut();
+        } else if (scrollPosition > babyParagraphPos && scrollPosition < rainbowParagraphPos || scrollPosition > babyParagraphPos && scrollPosition < bookParagraphPos) {
+            $(babyPhoto).fadeOut();
+            $(rainbowPhoto).fadeIn();
+            $(bookPhoto).fadeOut();
+            
+        } else if (scrollPosition > bookParagraphPos) {
+            $(rainbowPhoto).fadeOut();
+            $(bookPhoto).fadeIn();
+            $(babyPhoto).fadeOut();
+        }
+    
+    }
+};
+
